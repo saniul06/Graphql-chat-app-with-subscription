6c0a6a82718f8a0d4cdb240d8a423b053038c73d
saniul06 <saniul428@gmail.com>
2023-06-01 12:07:40 +0600

    Add subscription for message

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,8 @@
 import { GraphQLError } from 'graphql';
 import { createMessage, getMessages } from './db/messages.js';
+import { PubSub } from 'graphql-subscriptions';
+
+const pubSub = new PubSub();
 
 export const resolvers = {
   Query: {
@@ -10,11 +13,23 @@ export const resolvers = {
   },
 
   Mutation: {
-    addMessage: (_root, { text }, { user }) => {
+    addMessage: async (_root, { text }, { user }) => {
       if (!user) throw unauthorizedError();
-      return createMessage(user, text);
+      const message = createMessage(user, text);
+      pubSub.publish("MESSAGE_ADDED", { messageAdded: message });
+      return message;
     },
   },
+  Subscription: {
+    messageAdded: {
+      subscribe: (_root, _args, context) => {
+        if (!context?.user) {
+          if (!user) throw unauthorizedError();
+        }
+        return pubSub.asyncIterator("MESSAGE_ADDED")
+      }
+    }
+  }
 };
 
 function unauthorizedError() {
